4348a11fc47c35533abde53bdf57c6dba7419b6c
Peter Guntermann <pguntermann@andrena.de>
2020-08-15 19:39:03 +0200

    Install PDFMake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+// Create PDF tutorial:
+// https://www.ngdevelop.tech/angular-8-export-to-pdf-using-pdfmake/
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+             selector: 'app-root',
+             templateUrl: './app.component.html',
+             styleUrls: ['./app.component.scss']
+           })
 export class AppComponent {
-  title = 'song-sheet-maker';
+  private readonly documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+  
+  downloadPdf() {
+    pdfMake.createPdf(this.documentDefinition).download();
+  }
+
+  openPdfInNewWindow() {
+    pdfMake.createPdf(this.documentDefinition).open();
+  }
+
+  openPdfInSameWindow() {
+    pdfMake.createPdf(this.documentDefinition).open({}, window);
+  }
+
+  printPdf() {
+    pdfMake.createPdf(this.documentDefinition).print();
+  }
+
 }
